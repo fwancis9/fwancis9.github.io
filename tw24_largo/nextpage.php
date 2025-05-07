0c657b8af98a7078533e62e94c1b06ac233c977d
fwancis9 <marcfrancislargo08@gmail.com>
2025-05-07 11:12:42 +0800

    php added

---
+++ tw24_largo/nextpage.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+
+
+echo $_SERVER["REQUEST_METHOD"] ;
+
+// if ($_GET) {
+//     $var1 = $_GET['q'];
+//     $var2 = $_GET['s'];
+
+//     echo "GET";
+// } else {
+//     $var1 = $_POST['q'];
+//     $var2 = $_POST['s'];
+
+//     echo "POST";
+// }
+
+// echo ': '. $var1 . $var2;
+
+?>
+</body>
+</html>
